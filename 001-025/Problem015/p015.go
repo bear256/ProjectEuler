2f8f6816722ea71c62dde7e8b56a0e7077941803
bear256 <bear256@qq.com>
2015-09-24 17:47:05 +0800

    Update Problem 015

--- 001-025/Problem015/p015.go
+++ 001-025/Problem015/p015.go
@@ -4,6 +4,22 @@ import (
 	"fmt"
 )
 
+var sum int = 0
+
+func plus(na, nb []int) []int {
+	return []int{na[0]+nb[0],na[1]+nb[1]}
+}
+
+func count(root []int, dg int) int {
+	if root[0] == dg || root[1] == dg {
+		sum ++
+		fmt.Println(sum)
+		return 1
+	} else {
+		return count(plus(root, []int{0, 1}), dg) + count(plus(root, []int{1, 0}), dg)
+	}
+}
+
 func main() {
-	
+	fmt.Println(count([]int{0,0}, 4))
 }
