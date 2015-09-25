0cce181a123cc7b59a5c292b5321874bf4298377
bear256 <bear256@qq.com>
2015-09-25 17:46:20 +0800

    Update Solution 015

--- 001-025/Problem015/p015.go
+++ 001-025/Problem015/p015.go
@@ -20,6 +20,38 @@ func count(root []int, dg int) int {
 	}
 }
 
+func solution1Search(dg int) {
+	fmt.Println(count([]int{0,0}, dg))
+}
+
+func calcSum(array []int) int {
+	for ;len(array) > 1;{
+		tmp := make([]int, len(array)-1)
+		for i := 0; i < len(array)-1; i++ {
+			tmp[i] = array[i] + array[i+1]
+		}
+		array = tmp
+	}
+	return array[0]
+}
+
+func solution2Construct(dg int) {
+	array := []int{2}
+	if dg > 1{
+		for i := 2; i <= dg; i++ {	
+			tmp := array
+			array = make([]int, i)
+			array[0] = i+1
+			array[i-1] = i+1
+			for j := 1; j < i-1; j++ {
+				array[j] = tmp[j-1]+tmp[j]
+			}
+		}
+	}
+	fmt.Println(calcSum(array))
+}
+
 func main() {
-	fmt.Println(count([]int{0,0}, 4))
+	// solution1Search(20)
+	solution2Construct(20)
 }
